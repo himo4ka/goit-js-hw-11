c61e277241e1f144eeb63118a0d2acdd3d9d9387
Marina Baronina <baronina.mi@gmail.com>
2023-02-26 16:56:34 +0200

    remade

--- src/js/index.js
+++ src/js/index.js
@@ -1,91 +1,126 @@
-import { fetchImages } from './fetchImages';
+import PixabayApi from './fetchImages.js';
 import Notiflix from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
+const form = document.getElementById('search-form');
 const input = document.querySelector('.search-form-input');
-const btnSearch = document.querySelector('.search-form-button');
+
+// const btnSearch = document.querySelector('.search-form-button');
 const gallery = document.querySelector('.gallery');
 const btnLoadMore = document.querySelector('.load-more');
 let gallerySimpleLightbox = new SimpleLightbox('.gallery a');
 
+const pixabayApiService = new PixabayApi();
+
 btnLoadMore.style.display = 'none';
+form.addEventListener('submit', onSubmit);
+btnLoadMore.addEventListener('click', fetchPic);
 
-let pageNumber = 1;
+function onSubmit(e) {
 
-btnSearch.addEventListener('click', e => {
   e.preventDefault();
   cleanGallery();
-  const trimmedValue = input.value.trim();
-  if (trimmedValue !== '') {
-    fetchImages(trimmedValue, pageNumber).then(foundData => {
-      if (foundData.hits.length === 0) {
-        Notiflix.Notify.failure(
-          'Sorry, there are no images matching your search query. Please try again.'
-        );
-      } else {
-        renderImageList(foundData.hits);
-        Notiflix.Notify.success(
-          `Hooray! We found ${foundData.totalHits} images.`
-        );
-        btnLoadMore.style.display = 'block';
-        gallerySimpleLightbox.refresh();
-      }
-    });
+
+  const form = e.currentTarget;
+  const value = input.value.trim();
+
+  if (value.length === 0) {
+    return Notiflix.Notify.failure(`You've entered nothing. Please try again.`);
+  };
+  pixabayApiService.resetPage();
+  
+  pixabayApiService.query = value;
+
+  fetchPic();
+};
+
+async function fetchPic() {
+
+  try {
+    const data = await pixabayApiService.getImage();
+
+    const { hits, totalHits } = data;
+
+    if (hits.length === 0) throw new Error(onNothingFound());
+
+    const markup = hits.reduce(
+      (markup, hits) => createMarkup(hits) + markup,
+      ''
+    );
+    appendNewToList(markup);
+
+    let page = pixabayApiService.page - 1;
+
+    if (pixabayApiService.page - 1 === 1) onInfo(totalHits);
+    const totalPages = totalHits / 40;
+
+    if (page > totalPages) throw new Error(onNoMore());
+
+    btnLoadMore.style.display = 'block';
+  } catch (err) {
+    return err;
   }
-});
+}
 
-btnLoadMore.addEventListener('click', () => {
-  pageNumber++;
-  const trimmedValue = input.value.trim();
-  btnLoadMore.style.display = 'none';
-  fetchImages(trimmedValue, pageNumber).then(foundData => {
-    if (foundData.hits.length === 0) {
-      Notiflix.Notify.failure(
-        'Sorry, there are no images matching your search query. Please try again.'
-      );
-    } else {
-      renderImageList(foundData.hits);
-      Notiflix.Notify.success(
-        `Hooray! We found ${foundData.totalHits} images.`
-      );
-      btnLoadMore.style.display = 'block';
-      gallerySimpleLightbox.refresh();
-    }
-  });
-});
+function appendNewToList(markup) {
+  gallery.insertAdjacentHTML('beforeend', markup);
+  gallerySimpleLightbox.refresh();
+}
 
-function renderImageList(images) {
-  console.log(images, 'images');
-  const markup = images
-    .map(image => {
-      console.log('img', image);
+function createMarkup({
+  webformatURL,
+  largeImageURL,
+  tags,
+  likes,
+  views,
+  comments,
+  downloads,
+}) {
+  
       return `<div class="photo-card">
-
-      <a href="${image.largeImageURL}"><img class="photo" src="${image.webformatURL}" alt="${image.tags}" title="${image.tags}" loading="lazy"/></a>
-
+      <div class="images">
+      <a href="${largeImageURL}"><img class="photo" src="${webformatURL}" alt="${tags}" title="${tags}" loading="lazy"/></a>
+      </div>
         <div class="info">
             <p class="info-item">
-                <b>Likes</b> <span class="info-item-api"> ${image.likes} </span>
+                <b>Likes</b> <span class="info-item-api"> ${likes} </span>
             </p>
             <p class="info-item">
-                <b>Views</b> <span class="info-item-api">${image.views}</span>  
+                <b>Views</b> <span class="info-item-api">${views}</span>  
             </p>
             <p class="info-item">
-                <b>Comments</b> <span class="info-item-api">${image.comments}</span>  
+                <b>Comments</b> <span class="info-item-api">${comments}</span>  
             </p>
             <p class="info-item">
-                <b>Downloads</b> <span class="info-item-api">${image.downloads}</span> 
+                <b>Downloads</b> <span class="info-item-api">${downloads}</span> 
             </p>
         </div>
     </div>`;
-    })
-    .join('');
-  gallery.insertAdjacentHTML('beforeend', markup);
+    
 }
 
 function cleanGallery() {
   gallery.innerHTML = '';
-  pageNumber = 1;
   btnLoadMore.style.display = 'none';
+}
+
+function onNothingFound(err) {
+  btnLoadMore.style.display = 'none';
+  Notiflix.Notify.failure(
+    'Sorry, there are no images matching your search query. Please try again.'
+  );
+}
+
+function onNoMore() {
+  btnLoadMore.style.display = 'none';
+  Notiflix.Notify.info(
+  "That's all! We couldn't find more pictures..."
+  );
+}
+
+function onInfo(info) {
+  Notiflix.Notify.success(
+    `Hooray! We found ${info} images.`
+  );
 }
